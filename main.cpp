1a87de818a417972dadc18899b2cd6a77abd8110
Maximus Boxill <75922968+maxitaxi03@users.noreply.github.com>
2022-01-06 10:05:27 -0400

    create_account() works as expected

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include <array>
 #include <iomanip>
 #include <fstream>
+#include <string>
+#include <sstream>
 #include "bank_account.h"
 using namespace std;
 
@@ -17,7 +19,7 @@ array<Account, 25> create_account();
 int main(int argc, const char * argv[]) {
     // insert code here...
     Account maximus("Maximus", "Boxill", 19);
-//    array<Account, 25> arr = create_account();
+    array <Account, 25> arr = create_account();
     
     ofstream output;
     output.open("/Users/maximus/Library/Mobile Documents/com~apple~CloudDocs/Projects/Misc/bankAccount/bankAccount.txt");
@@ -32,15 +34,17 @@ int main(int argc, const char * argv[]) {
     output << left << setw(10) << "FName" << setw(10) << "LName" << setw(10) << "Age" << setw(10) << "Balance" << setw(10) << "Account Number" << endl;
     output << left << setw(10) <<maximus.get_fname() << setw(10) << maximus.get_lname() << setw(10) << maximus.get_age() << setw(10) << maximus.get_currBalance() << setw(10) << maximus.get_accountNumber() << endl;
     output.close();
-//    Account acct = arr.front();
-//    acct.print();
+    Account acct = arr.front();
+    acct.print();
     return 0;
 }
 
 
-array<Account, 25> create_account() {
+array <Account, 25> create_account() {
     ifstream input;
     array<Account, 25> account_container;
+//    string account_details_container[25];
+//    int length = sizeof(account_details_container) / sizeof(account_details_container[0]);
     input.open("/Users/maximus/Library/Mobile Documents/com~apple~CloudDocs/Projects/Misc/bankAccount/names.txt");
 
     if (input.fail()) {
@@ -48,12 +52,16 @@ array<Account, 25> create_account() {
         exit(1);
     }
     else {
-        string fname, lname, space1, space2;
-        int age;
-
+        string fname, lname, age;
+        int new_age = 0;
+        
+        
+        
         while (!input.eof()) {
-            input >> fname >> space1 >> lname >> space2 >> age;
-            Account account_details(fname, lname, age);
+            input >> fname >> lname >> age;
+            stringstream stream(age);
+            stream >> new_age;
+            Account account_details(fname, lname, new_age);
             account_container.fill(account_details);
         }
     }
